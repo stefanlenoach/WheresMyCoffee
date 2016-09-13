abba362a07b0b43bf128bf4d5e5acb83a4842716
Stefan Le Noach <stefanlenoach@gmail.com>
2016-09-13 17:19:48 -0400

    added directories

---
+++ app/components/root.js
@@ -0,0 +1,90 @@
+import React, { Component } from 'react';
+import {AppRegistry,StyleSheet,Text,View,TouchableElement} from 'react-native';
+import Button from 'react-native-button'
+
+
+class WheresMyCoffee extends Component {
+  state = {
+    initialPosition: 'unknown',
+    lastPosition: 'unknown',
+  };
+
+  watchID: ?number = null;
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        var initialPosition = JSON.stringify(position);
+        this.setState({initialPosition});
+      },
+      (error) => alert(error),
+      {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000}
+    );
+    this.watchID = navigator.geolocation.watchPosition((position) => {
+      var lastPosition = JSON.stringify(position);
+      this.setState({lastPosition});
+    });
+  }
+
+  componentWillUnmount() {
+    navigator.geolocation.clearWatch(this.watchID);
+  }
+
+  constructURL() {
+
+  }
+
+  render() {
+    function getData(url){
+      fetch(url, {method: "GET"})
+      .then(function(response){
+        console.log(response)
+        return response;
+      })
+    }
+    // console.log(getData())
+    console.log(this.state.initialPosition)
+    console.log(this.state.lastPosition)
+
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit index.ios.js
+        </Text>
+        <Text style={styles.instructions}>
+          Press Cmd+R to reload,{'\n'}
+          Cmd+D or shake for dev menu
+        </Text>
+        <Button
+          style={{borderWidth: 1, borderColor: 'transparent', backgroundColor: 'mistyrose'}}
+          onPress={this._handleClick}>
+          Press Me!
+        </Button>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'coral',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+module.exports = WheresMyCoffee
