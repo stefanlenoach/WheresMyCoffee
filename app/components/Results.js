1bc4bd1777dd8c8d954c3c31c27533a1f601e72c
Stefan Le Noach <stefanlenoach@gmail.com>
2016-09-18 16:11:55 -0400

    styling

--- app/components/Results.js
+++ app/components/Results.js
@@ -45,8 +45,7 @@ class Results extends Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
+    justifyContent: 'space-around',
     alignItems: 'center',
     backgroundColor: 'coral',
   },
